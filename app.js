5d1e32c5ce92267c1bc28a2e17dde53bf2cfa647
mooonggu <bhnnjm1@naver.com>
2021-03-16 01:00:07 +0900

    ts test1

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// @ts-check
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+
+function sum(a, b) {
+    return a+ b;
+}
+
+sum(20, 20);
---
+++ test.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number):number {
+    return a+ b;
+}
+
+let result = sum(20,30)
+result.toLocaleString()
